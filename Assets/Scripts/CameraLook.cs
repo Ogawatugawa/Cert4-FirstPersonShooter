84015f48e32a487140678d7fed60c1eefe6ea452
Ogawatugawa <vin.tansiri@gmail.com>
2019-05-01 16:26:09 +1000

    Initial Commit

---
+++ Assets/Scripts/CameraLook.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    public bool isCursorHidden = true;
+    public float minPitch = -80f, maxPitch = 80f;
+    public Vector2 speed = new Vector2(120f, 120f); // Speed in degrees per second
+
+    private Vector2 euler; // Current euler rotation
+    void Start()
+    {
+        // Is the curson supposed to be hidden?
+        if (isCursorHidden)
+        {
+            // Lock and hide it
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        // Get current camera euler
+        euler = transform.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate the euler with mouse movement
+        euler.y += Input.GetAxis("Mouse X") * speed.x * Time.deltaTime;
+        euler.x -= Input.GetAxis("Mouse Y") * speed.y * Time.deltaTime;
+        // Clamp the camera on pitch
+        euler.x = Mathf.Clamp(euler.x, minPitch, maxPitch);
+        // Apply euler to the Player and Camera separately
+        transform.parent.localEulerAngles = new Vector3(0, euler.y, 0);
+        transform.localEulerAngles = new Vector3(euler.x, 0, 0);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Player : MonoBehaviour
+{
+    public float runSpeed = 8f;
+    public float walkSpeed = 6f, gravity = 10f, jumpHeight = 15f, groundRayDistance = 1.1f, sprintFactor = 2f;
+
+    private CharacterController charC;
+    private Vector3 motion; // Movement offset per frame
+
+    private void Start()
+    {
+        charC = GetComponent<CharacterController>();   
+    }
+
+    private void Update()
+    {
+        float inputH = Input.GetAxis("Horizontal");
+        float inputV = Input.GetAxis("Vertical");
+
+        //if (Input.GetKey(KeyCode.LeftShift))
+        //{
+        //    inputH *= sprintFactor;
+        //    inputV *= sprintFactor;
+        //}
+
+        Move(inputH, inputV);
+
+        if (IsGrounded())
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                motion.y = jumpHeight;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Sprint(inputH, inputV);
+        }
+
+        motion.y -= gravity * Time.deltaTime;
+        charC.Move(motion * Time.deltaTime);
+    }
+
+    bool IsGrounded()
+    {
+        // Raycast below the player
+        Ray groundRay = new Ray(transform.position, -transform.up);
+        RaycastHit hit;
+        // If hitting something
+        if (Physics.Raycast(groundRay, out hit, groundRayDistance))
+        {
+            // Return true
+            return true;
+        }
+        // Else
+        // Return false
+        return false;
+    }
+
+    private void Move(float inputH, float inputV)
+    {
+        Vector3 direction = new Vector3(inputH, 0f, inputV);
+
+        direction = transform.TransformDirection(direction);
+
+        float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed; 
+
+        motion.x = direction.x * currentSpeed;
+        motion.z = direction.z * currentSpeed;
+    }
+
+    void Sprint(float inputH, float inputV)
+    {
+        Vector3 direction = new Vector3(inputH, 0f, inputV);
+
+        direction = transform.TransformDirection(direction);
+
+        motion.x = direction.x * runSpeed;
+        motion.z = direction.z * runSpeed;
+    }
+
+    //#region Variables
+    //[Header("Movement")]
+    //public Vector3 moveDir;
+    //public float moveSpeed;
+    //public float jumpSpeed, gravity;
+    //public CharacterController charC;
+    //#endregion
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    charC = GetComponent<CharacterController>();
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    //    moveDir = transform.TransformDirection(moveDir);
+    //    moveDir *= moveSpeed*Time.deltaTime;
+    //    if (Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //        moveDir.y = jumpSpeed*Time.deltaTime;
+    //    }
+
+    //    moveDir.y -= gravity * Time.deltaTime;
+    //    charC.Move(moveDir);
+
+
+    //}
+}
